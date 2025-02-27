b358303edb321a83f29b6f8944421d17cb5bcd34
PUNEET BATRA <ishitabatra2004@gmail.com>
2025-02-27 20:33:56 +0530

    ghj

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Profile from "./components/Profile";
+import Profile from "./components/profile";
+import MoodFeed from './components/moodfeed';
 
 function App() {
     return (
+      
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
             </Routes>
+            <div>
+           <MoodFeed />
+        </div>
         </Router>
+        
+         
     );
 }
 
